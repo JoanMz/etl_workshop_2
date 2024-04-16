1bfb7c7abc4cdb083e769be4246cb0ea25459d4e
JoanMz <joanmzteo@gmail.com>
2024-04-16 14:18:28 -0500

    Docker works - EDA Grammys before merge

--- DB_Connection/Pysqlconnect.py
+++ DB_Connection/Pysqlconnect.py
@@ -1,1 +1,10 @@
 from decouple import config
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy import create_engine
+
+engine = create_engine(f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_BASE')}")
+base = declarative_base()
+
+def connection(engine=engine):
+    return engine
